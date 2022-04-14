f2244be3cfc834250673817a6d74064a3bf7deae
unknown <dakisya@gmail.com>
2022-04-14 23:41:17 +0300

    fin2

--- src/requestmapper.cpp
+++ src/requestmapper.cpp
@@ -33,53 +33,10 @@ void RequestMapper::service(HttpRequest& request, HttpResponse& response)
     QByteArray path=request.getPath();
     qDebug("RequestMapper: path=%s",path.data());
 
-    // For the following pathes, each request gets its own new instance of the related controller.
-
-/*    if (path.startsWith("/dump"))
-    {
-        DumpController().service(request, response);
-    }
-
-    else if (path.startsWith("/template"))
-    {
-        TemplateController().service(request, response);
-    }
-
-    else if (path.startsWith("/form"))
-    {
-        FormController().service(request, response);
-    }
-
-    else if (path.startsWith("/file"))
-    {
-        FileUploadController().service(request, response);
-    }
-
-    else if (path.startsWith("/session"))
-    {
-        SessionController().service(request, response);
-    }
-
-    else if (path.startsWith("/login"))
-    {
-        LoginController().service(request, response);
-    }
-
-    // All other pathes are mapped to the static file controller.
-    // In this case, a single instance is used for multiple requests.
-    else
-    {
-        staticFileController->service(request, response);
-    }
-*/
     if (path == "/" || path.startsWith("/main") || path.startsWith("/admin"))
     {
         TemplateController().service(request, response);
     }
-//    else if (path.startsWith("/admin"))
-//    {
-//       FormController().service(request, response);
-//    }
     else if (path == "/file")
     {
         FileUploadController().service(request, response);
